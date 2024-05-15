5482ac7b498400d6b235e9b8d2fa1df6509a826d
Kasper Nielsen <kasper0406@gmail.com>
2024-05-15 17:08:08 +0200

    Initial Commit

---
+++ PianoTranscriber/PianoTranscriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  PianoTranscriberApp.swift
+//  PianoTranscriber
+//
+//  Created by Kasper Nielsen on 15/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct PianoTranscriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
